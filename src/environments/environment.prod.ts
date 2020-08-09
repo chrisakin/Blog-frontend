ab12a9b06af48763e5c9c08a25478ebad94a46c1
Christopher Akintade <chrisfemide@gmail.com>
2020-08-09 15:02:46 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  articlesUrl: 'http://localhost:3000/articles',
+  articleUrl: 'http://localhost:3000/article/',
+  registerUrl: "http://localhost:3000/auth/register",
+  loginUrl: "http://localhost:3000/auth/sign_in",
 };
