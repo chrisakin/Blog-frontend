d11ac8722c31359eb6391d7c61ada151dd322521
Christopher Akintade <chrisfemide@gmail.com>
2020-08-09 18:05:00 +0100

    cors issue

--- src/app/service/article.service.ts
+++ src/app/service/article.service.ts
@@ -17,7 +17,9 @@ export class ArticleService {
       'Authorization': this.token,
       'Access-Control-Allow-Origin':'*',
       'Access-Control-Allow-Methods':'PUT, POST, GET, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept,',
      });
+     
   }
 
   getArticles(): Observable<Post> {
